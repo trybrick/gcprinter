953bf55e01f749920481e690515b83d7ed438b87
Tom <tom@noogen.net>
2016-06-13 10:48:18 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+DEST_BRANCH_DEFAULT=master
+DEST_BRANCH=${1:-$DEST_BRANCH_DEFAULT}
 
-aws s3 sync "./" "s3://brick-web/ds/$1/script/gcprinter"
+aws s3 sync "./" "s3://brick-web/ds/$DEST_BRANCH/script/gcprinter"
